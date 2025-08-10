9587cc9755524a309724295006435c08af66af79
IllyaShramko <123illya123123r@gmail.com>
2025-08-10 15:37:04 +0300

    fix

--- home_app/views.py
+++ home_app/views.py
@@ -1,10 +1,12 @@
 import flask, flask_login
+from library_app.models import Quiz
 
 def render_home_page():
 
     if flask_login.current_user.is_authenticated:
         return flask.redirect("/admin/")
-    
+    quizes= Quiz.query.all()[:4]
+    print(quizes)
     return flask.render_template(
         template_name_or_list= 'home.html'
     )
