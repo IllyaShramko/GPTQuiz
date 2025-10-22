abe056028bd6a9da90c9f18228305874763dac56
IllyaShramko <123illya123123r@gmail.com>
2025-10-22 21:01:11 +0300

    gevent

--- project/settings.py
+++ project/settings.py
@@ -1,17 +1,16 @@
-import flask
-import flask_migrate, flask_sqlalchemy
-from flask_socketio import SocketIO, emit
+import flask, flask_migrate, flask_sqlalchemy
+from flask_socketio import SocketIO
 
 project = flask.Flask(
-    import_name= 'project',
-    template_folder= 'templates',
-    static_folder= 'static'
+    import_name='project',
+    template_folder='templates',
+    static_folder='static'
 )
 project.config['SECRET_KEY'] = "secret"
 project.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
-DATABASE= flask_sqlalchemy.SQLAlchemy(app= project)
+DATABASE = flask_sqlalchemy.SQLAlchemy(app=project)
 
-migrate= flask_migrate.Migrate(app= project, db= DATABASE)
-socketio = SocketIO(app=project, async_mode='eventlet')
+migrate = flask_migrate.Migrate(app=project, db=DATABASE)
 
+socketio = SocketIO(app=project, async_mode='gevent')
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,4 @@
-print("Before any imports")
-import eventlet
-eventlet.monkey_patch()
-print("After monkey_patch")
-
-from project import project, socketio  
+from project import project, socketio
 
 if __name__ == "__main__":
-    socketio.run(project, host="0.0.0.0", port=8080, debug=False)
+    socketio.run(project, host="0.0.0.0", port=8080)
