db2a037b7c26c385688d9f4b9f052fe5113a3af8
IllyaShramko <123illya123123r@gmail.com>
2025-10-22 21:23:28 +0300

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python wsgi.py
+web: gunicorn -w 1 -b 0.0.0.0:$PORT wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
 
-project = Flask(__name__)
+app = Flask(__name__)
 
-@project.route('/')
+@app.route('/')
 def index():
-    return "Hello World! Railway test works!"
+    return "Hello World!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    project.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 8080)) 
+    app.run(host="0.0.0.0", port=port)
