b0ba523c2ad591480af7d0819e364c01a76b0f22
IllyaShramko <123illya123123r@gmail.com>
2025-10-22 21:31:04 +0300

    .

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask
+from flask_socketio import SocketIO
 
-app = Flask(__name__)
+project = Flask(__name__)
 
-@app.route('/')
+@project.route('/')
 def index():
-    print("Index page accessed")
-    return "Hello World!"
+    return "Hello World! SocketIO works!"
+
+socketio = SocketIO(app=project, async_mode='gevent')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080)) 
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    socketio.run(project, host="0.0.0.0", port=port)
