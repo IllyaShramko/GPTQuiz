8f4906c0f07ba804982515f065e7f4375c1f6f19
IllyaShramko <123illya123123r@gmail.com>
2025-10-22 21:36:03 +0300

    1

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_socketio import SocketIO
 
@@ -9,7 +8,3 @@ def index():
     return "Hello World! SocketIO works!"
 
 socketio = SocketIO(app=project, async_mode='gevent')
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    socketio.run(project, host="0.0.0.0", port=port)
