49c3208710ccc76136e936d8dd3a495a1558847e
IllyaShramko <123illya123123r@gmail.com>
2025-10-22 20:50:52 +0300

    test

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+import eventlet
+eventlet.monkey_patch()  
+
+from project import project, socketio  
+
+if __name__ == "__main__":
+    socketio.run(project, host="0.0.0.0", port=8080, debug=False)
