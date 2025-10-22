b399cf6567db5b9f1b4b1afbd76ea4ee8e4e5e3c
IllyaShramko <123illya123123r@gmail.com>
2025-10-22 21:11:11 +0300

    fix

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,7 @@
-import flask, flask_migrate, flask_sqlalchemy
+import flask
 from flask_socketio import SocketIO
+import flask_sqlalchemy
+import flask_migrate
 
 project = flask.Flask(
     import_name='project',
@@ -10,7 +12,7 @@ project.config['SECRET_KEY'] = "secret"
 project.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 DATABASE = flask_sqlalchemy.SQLAlchemy(app=project)
-
 migrate = flask_migrate.Migrate(app=project, db=DATABASE)
 
 socketio = SocketIO(app=project, async_mode='gevent')
+
