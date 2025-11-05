299fec40c3f34c62b5237e57de70160282a814f7
IllyaShramko <123illya123123r@gmail.com>
2025-11-05 23:09:46 +0200

    Try 465 port

--- project/flask_config.py
+++ project/flask_config.py
@@ -6,9 +6,9 @@ import os
 load_dotenv()
 
 project.config["MAIL_SERVER"] = "smtp.gmail.com"
-project.config["MAIL_PORT"] = 587 
-project.config["MAIL_USE_TLS"] = True
-project.config["MAIL_USE_SSL"] = False
+project.config["MAIL_PORT"] = 465
+project.config["MAIL_USE_TLS"] = False
+project.config["MAIL_USE_SSL"] = True
 project.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 project.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 project.config["MAIL_DEFAULT_SENDER"] = ("GPTQuiz", "test.python.1488@gmail.com")
