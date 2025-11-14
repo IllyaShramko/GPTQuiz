e36c5b9941ed32d98af02623aa4a50de1aa6d0c6
IllyaShramko <123illya123123r@gmail.com>
2025-11-14 21:21:16 +0200

    Added adaptaion for phone 1part

--- home_app/static/js/scroll.js
+++ home_app/static/js/scroll.js
@@ -4,9 +4,12 @@ const rightBtn = document.querySelector('.scroll-right-btn')
 
 leftBtn.onclick = () => {
     quizesContainer.scrollBy({ left: -238, behavior: 'smooth' })
+    setTimeout(updateButtons, 300)
 }
+
 rightBtn.onclick = () => {
     quizesContainer.scrollBy({ left: 238, behavior: 'smooth' })
+    setTimeout(updateButtons, 300)
 }
 
 let scrollAmount = 0
@@ -29,4 +32,14 @@ function smoothScroll() {
         isScrolling = false
         scrollAmount = 0
     }
+    updateButtons()
 }
+
+function updateButtons() {
+    const maxScroll = quizesContainer.scrollWidth - quizesContainer.clientWidth
+    leftBtn.style.display = quizesContainer.scrollLeft <= 0 ? 'none' : 'block'
+    rightBtn.style.display = quizesContainer.scrollLeft >= maxScroll - 1 ? 'none' : 'block'
+}
+
+quizesContainer.addEventListener('scroll', updateButtons)
+window.addEventListener('load', updateButtons)
