f34fe5b72a576a46606b24a86198587f049f6fdf
IllyaShramko <123illya123123r@gmail.com>
2026-01-28 20:35:38 +0200

    Validate auth

--- execution_app/views.py
+++ execution_app/views.py
@@ -9,26 +9,24 @@ def render_login_student():
         for student in Student.query.filter_by(login = flask.request.form['login']):
             if student.password == flask.request.form['password']:
                 flask_login.login_user(student)
+                flask.session['user_role'] = 'student'
                 return flask.redirect('/execution/')
     return flask.render_template(template_name_or_list= 'login_student.html')
+
 def render_enter_code():
     if not flask_login.current_user.is_authenticated:
         return flask.redirect("/login_student/")
-    if not flask_login.current_user.is_student:
+    if flask.session.get("user_role") != "student":
         return flask.redirect("/login_student/")
     
     code = flask.request.args.get("code")
     if code:
         quiz = RedeemCode.query.filter_by(code_enter= code).first_or_404().quiz
-        print(quiz)
-        username = ""
-        if flask_login.current_user.is_authenticated:
-            username = flask_login.current_user.name + " " + flask_login.current_user.surname
         room = RedeemCode.query.filter_by(code_enter=code).first().room
         teacher = User.query.get(room.host)
         return flask.render_template(
             template_name_or_list= 'enter_nickname.html',
-            quiz=quiz, username=username, teacher=teacher
+            quiz=quiz, teacher=teacher
         )
     return flask.render_template(
         template_name_or_list="enter_code.html"
--- home_app/views.py
+++ home_app/views.py
@@ -4,9 +4,11 @@ from library_app.models import Quiz
 def render_home_page():
 
     if flask_login.current_user.is_authenticated:
-        if flask_login.current_user.is_student:
+        role = flask.session.get('user_role') 
+        if role == 'teacher':
+            return flask.redirect("/admin/")
+        if role == 'student':
             return flask.redirect("/student/")
-        return flask.redirect("/admin/")
     quizes= Quiz.query.filter_by(is_draft = False).all()
     print(quizes)
     return flask.render_template(
