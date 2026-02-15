88491d6e3fda00b74ed162574e39167925d92040
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2026-02-15 20:46:10 +0200

    update switch

--- reports_app/static/js/switch.js
+++ reports_app/static/js/switch.js
@@ -1,33 +1,40 @@
-const graphicBtn = document.getElementById("graphic")
-const generalBtn = document.getElementById("general")
-const studentsInfo = document.querySelector(".judah")
-const graphicInfo = document.querySelector(".chart-container")
-let current_page = "general"
+const graphicBtn = document.getElementById("graphic");
+const generalBtn = document.getElementById("general");
+const studentsInfo = document.querySelector(".judah");
+const graphicInfo = document.querySelector(".chart-container");
+let current_page = "general";
 const roomId = document.getElementById('room_id').value;
+const questionsBtn = document.getElementById('questions');
 
-graphicBtn.addEventListener("click", ()=>{
-    if (current_page == "graphic") {
-        return
+
+function switchTab(tab) {
+    if (current_page === tab) return;
+
+    if (tab === "graphic") {
+        graphicBtn.classList.remove("not");
+        graphicBtn.classList.add('selected');
+        graphicInfo.classList.remove("hide");
+
+        generalBtn.classList.remove("selected");
+        generalBtn.classList.add("not");
+        studentsInfo.classList.add("hide");
+
+        loadRoomStats(roomId);
+    } else if (tab === "general") {
+        generalBtn.classList.remove("not");
+        generalBtn.classList.add('selected');
+        studentsInfo.classList.remove("hide");
+
+        graphicBtn.classList.remove("selected");
+        graphicBtn.classList.add("not");
+        graphicInfo.classList.add("hide");
+    } else if (tab === "questions") {
+        
     }
-    graphicBtn.classList.remove("not")
-    graphicBtn.classList.add('selected')
-    graphicInfo.classList.remove("hide")
-    generalBtn.classList.remove("selected")
-    generalBtn.classList.add("not")
-    studentsInfo.classList.add("hide")
-    console.log(roomId)
-    loadRoomStats(roomId);
-    current_page = "graphic"
-})
-generalBtn.addEventListener("click", ()=>{
-    if (current_page == "general") {
-        return
-    }
-    generalBtn.classList.remove("not")
-    generalBtn.classList.add('selected')
-    studentsInfo.classList.remove("hide")
-    graphicBtn.classList.remove("selected")
-    graphicBtn.classList.add("not")
-    graphicInfo.classList.add("hide")
-    current_page = "general"
-})
+
+    current_page = tab;
+}
+
+graphicBtn.addEventListener("click", () => switchTab("graphic"));
+generalBtn.addEventListener("click", () => switchTab("general"));
+questionsBtn.addEventListener("click", () => switchTab("questions"));
