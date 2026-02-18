e2d92b0e0943eed185bdb6d5a510ddee7239dd82
Artem Svistun <139866643+asvistun5@users.noreply.github.com>
2026-02-18 20:39:16 +0200

    update tabs

--- reports_app/static/js/switch.js
+++ reports_app/static/js/switch.js
@@ -1,40 +1,30 @@
-const graphicBtn = document.getElementById("graphic");
-const generalBtn = document.getElementById("general");
-const studentsInfo = document.querySelector(".judah");
-const graphicInfo = document.querySelector(".chart-container");
-let current_page = "general";
-const roomId = document.getElementById('room_id').value;
-const questionsBtn = document.getElementById('questions');
+const tabs = document.querySelectorAll(".tabs button");
+const contents = document.querySelectorAll(".tab-content");
+const roomId = document.getElementById("room_id").value;
+
+let currentPage = "general";
 
 
 function switchTab(tab) {
-    if (current_page === tab) return;
+    if (tab === currentPage) return;
 
+
+    tabs.forEach(btn => {
+        btn.classList.toggle("selected", btn.dataset.tab === tab);
+        btn.classList.toggle("not", btn.dataset.tab !== tab);
+    });
+    contents.forEach(tg => {
+        tg.classList.toggle("hide", tg.dataset.content !== tab);
+    });
+
+
+    /* judah tut */
     if (tab === "graphic") {
-        graphicBtn.classList.remove("not");
-        graphicBtn.classList.add('selected');
-        graphicInfo.classList.remove("hide");
-
-        generalBtn.classList.remove("selected");
-        generalBtn.classList.add("not");
-        studentsInfo.classList.add("hide");
-
         loadRoomStats(roomId);
-    } else if (tab === "general") {
-        generalBtn.classList.remove("not");
-        generalBtn.classList.add('selected');
-        studentsInfo.classList.remove("hide");
-
-        graphicBtn.classList.remove("selected");
-        graphicBtn.classList.add("not");
-        graphicInfo.classList.add("hide");
-    } else if (tab === "questions") {
-        
     }
 
-    current_page = tab;
+    currentPage = tab;
 }
 
-graphicBtn.addEventListener("click", () => switchTab("graphic"));
-generalBtn.addEventListener("click", () => switchTab("general"));
-questionsBtn.addEventListener("click", () => switchTab("questions"));
+
+tabs.forEach(btn => btn.addEventListener("click", () => switchTab(btn.dataset.tab)))
